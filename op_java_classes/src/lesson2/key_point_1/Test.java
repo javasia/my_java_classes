eaa473d1a92aff101468e00b981416ba475a078d
javasia <impex.ua@gmail.com>
2016-02-06 22:14:06 +0200

    cleanup
    deleted files not necessary for a project

---
+++ op_java_classes/src/lesson2/key_point_1/Test.java
@@ -0,0 +1,14 @@
+package lesson2.key_point_1;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		double d = 25.0;
+		int i = 25;
+		System.out.println(d==i);
+		System.out.println(10<8);
+		
+	}
+
+}

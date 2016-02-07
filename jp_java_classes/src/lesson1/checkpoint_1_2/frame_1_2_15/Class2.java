bd58363506635d159d720d9ce30a290c8a0489cf
javasia <impex.ua@gmail.com>
2016-02-07 03:26:07 +0200

    Frame_1_2_15
    circled references between objects

---
+++ jp_java_classes/src/lesson1/checkpoint_1_2/frame_1_2_15/Class2.java
@@ -0,0 +1,7 @@
+package lesson1.checkpoint_1_2.frame_1_2_15;
+
+public class Class2 {
+	public String class2 = "class3";
+	Class1 obj1 = new Class1();
+	public String class1 = obj1.class1;
+}
---
+++ jp_java_classes/src/lesson1/checkpoint_1_2/frame_1_2_15/Class3.java
@@ -0,0 +1,7 @@
+package lesson1.checkpoint_1_2.frame_1_2_15;
+
+public class Class3 {
+	public String class3 = "class3";
+	Class2 obj2 = new Class2();
+	public String class2 = obj2.class2;
+}

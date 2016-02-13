2c1e72ed8e706223f049147800c02ad95455189e
javasia <impex.ua@gmail.com>
2016-02-13 22:49:01 +0200

    frame_1_3_4
    previous was incorrect

--- jp_java_classes/src/lesson1/checkpoint_1_3/frame_1_3_4/Bullet.java
+++ jp_java_classes/src/lesson1/checkpoint_1_3/frame_1_3_4/Bullet.java
@@ -1,12 +1,35 @@
 package lesson1.checkpoint_1_3.frame_1_3_4;
 
 public class Bullet {
-
+	
 	int x;
 	int y;
 	int speed;
 	int direction;
-			
+
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public int getDirection() {
+		return direction;
+	}
+	
+	public void updateX (int modifier) {
+		x+=modifier;
+	}
+	public void updateY (int modifier) {
+		y+=modifier;
+	}
+	public void destroy (){
+		x=-100;
+		y=-100;
+	}
 }
 
 

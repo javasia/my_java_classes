10c7cee261e7b2158d1f085f2cd99626270d3f95
javasia <impex.ua@gmail.com>
2016-02-17 21:33:34 +0200

    Frame_1_4_4
    Enum created

---
+++ jp_java_classes/src/lesson1/checkpoint_1_4/frame_1_4_4/Color.java
@@ -0,0 +1,6 @@
+package lesson1.checkpoint_1_4.frame_1_4_4;
+
+public enum Color {
+BLACK, BLUE, RED, GREEN, YELLOW, PINK, ORANGE, BROWN, WHITE, GREY, AUBERGINE, 
+DARK_BLUE,DARK_RED, DARK_GREEN, LIGHT_BLUE, LIGHT_GREEN; 
+}

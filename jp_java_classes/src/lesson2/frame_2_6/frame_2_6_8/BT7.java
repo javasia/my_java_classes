55ce64a61975b5c6db99a704d2c169041d790288
javasia <impex.ua@gmail.com>
2016-03-20 15:54:37 +0200

    Frame_2_6_8
    Defender+Agressor+NewProcessInterception

---
+++ jp_java_classes/src/lesson2/frame_2_6/frame_2_6_8/BT7.java
@@ -0,0 +1,10 @@
+package lesson2.frame_2_6.frame_2_6_8;
+
+public class BT7 extends Tank{
+	public BT7(ActionField af, BattleField bf, Type type) {
+		super(af, bf, type);
+		setColor(Colors.GREEN);
+		setCrew(5);
+		setSpeed(getSpeed()*2);	
+	}
+}
---
+++ jp_java_classes/src/lesson2/frame_2_6/frame_2_6_8/Colors.java
@@ -0,0 +1,5 @@
+package lesson2.frame_2_6.frame_2_6_8;
+
+public enum Colors {
+BLUE, RED, GREEN, YELLOW;
+}

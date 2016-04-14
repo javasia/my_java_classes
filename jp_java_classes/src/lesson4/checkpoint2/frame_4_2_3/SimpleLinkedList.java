7aa60da7834417318abd3bfa1b4eca58145c74ac
javasia <impex.ua@gmail.com>
2016-04-14 23:01:37 +0300

    Frame_4_2_3
    INNER CLASS

---
+++ jp_java_classes/src/lesson4/checkpoint2/frame_4_2_3/SimpleLinkedList.java
@@ -0,0 +1,27 @@
+package lesson4.checkpoint2.frame_4_2_3;
+
+public class SimpleLinkedList {
+	Node root;
+	int size;
+	
+	public void addFirst(Object o){
+		
+	}
+	
+	public void addLast(Object o){
+		
+	}
+	
+	public  void addAfter(Object o, Object prev){
+		
+	}
+	
+	public  int getSize(){
+		return size;
+	}
+	
+	public class Node {
+		Object o;
+		Node node;
+	}
+}

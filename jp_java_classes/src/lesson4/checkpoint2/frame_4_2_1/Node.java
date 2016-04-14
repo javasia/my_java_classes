c29f94c7d9223233d26e1316d8d963603720dcd9
javasia <impex.ua@gmail.com>
2016-04-14 22:55:36 +0300

    Frame_4_2_1

--- jp_java_classes/src/lesson4/checkpoint2/frame_4_2_1/Node.java
+++ jp_java_classes/src/lesson4/checkpoint2/frame_4_2_1/Node.java
@@ -1,5 +1,6 @@
 package lesson4.checkpoint2.frame_4_2_1;
 
 public class Node {
-
+	Object o;
+	Node node;
 }
--- jp_java_classes/src/lesson4/checkpoint2/frame_4_2_1/SimpleLinkedList.java
+++ jp_java_classes/src/lesson4/checkpoint2/frame_4_2_1/SimpleLinkedList.java
@@ -1,5 +1,24 @@
 package lesson4.checkpoint2.frame_4_2_1;
 
 public class SimpleLinkedList {
-
+	Node root;
+	int size;
+	
+	public void addFirst(Object o){
+		
+	}
+	
+	public void addLast(Object o){
+		
+	}
+	
+	public  void addAfter(Object o, Object prev){
+		
+	}
+	
+	public  int getSize(){
+		return size;
+	}
+	
+	
 }

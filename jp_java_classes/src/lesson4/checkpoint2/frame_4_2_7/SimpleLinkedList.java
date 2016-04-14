ca1e2124ae7bb43da715213431d5a798e2f27949
javasia <impex.ua@gmail.com>
2016-04-14 23:38:04 +0300

    Frame_4_2_7
    null check added

--- jp_java_classes/src/lesson4/checkpoint2/frame_4_2_7/SimpleLinkedList.java
+++ jp_java_classes/src/lesson4/checkpoint2/frame_4_2_7/SimpleLinkedList.java
@@ -3,33 +3,37 @@ package lesson4.checkpoint2.frame_4_2_7;
 public class SimpleLinkedList {
 	Node root;
 	int size;
-	
-	public void addFirst(Object obj){
+
+	public void addFirst(Object obj) {
 		Node node = new Node();
-		node.obj=obj;
-		if (root!=null){
-			node.node=root;
+		node.obj = obj;
+		if (root != null) {
+			node.node = root;
 		}
-		root=node;
+		root = node;
 		size++;
 	}
-	
-	public void addLast(Object obj){
-		Node node = new Node();
-		node.obj=obj;
-		node.node=root;
-		root.node=node;
-		size++;
+
+	public void addLast(Object obj) {
+		if (root == null) {
+			addFirst(obj);
+		} else {
+			Node node = new Node();
+			node.obj = obj;
+			node.node = root;
+			root.node = node;
+			size++;
+		}
 	}
-	
-	public  void addAfter(Object o, Object prev){
-		
+
+	public void addAfter(Object o, Object prev) {
+
 	}
-	
-	public  int getSize(){
+
+	public int getSize() {
 		return size;
 	}
-	
+
 	private class Node {
 		Object obj;
 		Node node;

3acd1a6b4384ca1830d9cd50d1c0c2ff337e3355
javasia <impex.ua@gmail.com>
2016-04-17 23:09:03 +0300

    Frame_4_2_16
    amended

--- jp_java_classes/src/lesson4/checkpoint2/frame_4_2_16/MyComparator.java
+++ jp_java_classes/src/lesson4/checkpoint2/frame_4_2_16/MyComparator.java
@@ -9,9 +9,9 @@ public class MyComparator implements Comparator<String> {
 		char ch1 = o1.charAt(0);
 		char ch2 = o2.charAt(0);
 
-		if (ch1 < ch2) {
+		if (ch1 > ch2) {
 			return -1;
-		} else if (ch1 > ch2) {
+		} else if (ch1 < ch2) {
 			return 1;
 		}
 		return 0;
